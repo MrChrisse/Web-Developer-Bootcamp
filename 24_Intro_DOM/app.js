29fa3622ae6e834bd384eca2457da4fad9bef216
MrChrisse <chrilei2000@gmail.com>
2021-05-24 16:22:37 +0200

    continued chapter24

--- 24_Intro_DOM/app.js
+++ 24_Intro_DOM/app.js
@@ -3,3 +3,6 @@ const allImages = document.getElementsByTagName('img');
 for (let img of allImages) {
   img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg';
 }
+
+const firstPara = document.querySelector('p');
+const allPara = document.querySelectorAll('p');
