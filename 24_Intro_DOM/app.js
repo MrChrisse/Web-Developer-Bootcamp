b1fa3190b383ad5070912b1469e88b81fd63d75c
MrChrisse <chrilei2000@gmail.com>
2021-05-27 09:15:23 +0200

    continued chapter24

--- 24_Intro_DOM/app.js
+++ 24_Intro_DOM/app.js
@@ -6,3 +6,22 @@ for (let img of allImages) {
 
 const firstPara = document.querySelector('p');
 const allPara = document.querySelectorAll('p');
+
+const allLinks = document.querySelectorAll('a');
+
+for (let link of allLinks) {
+  link.style.color = 'green';
+  link.style.textDecorationColor = 'magenta';
+  link.style.textDecorationStyle = 'wavy';
+}
+
+window.getComputedStyle(h1);
+
+const h2 = document.querySelector('h2');
+h2.classList.add('tocnumber');
+
+const lis = document.querySelectorAll('li');
+
+for (let el of lis) {
+  el.classList.toggle('highlight');
+}
