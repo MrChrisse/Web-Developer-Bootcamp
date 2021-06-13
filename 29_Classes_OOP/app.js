792acbfe1b2a1af250a8bb7fa4e5937892915094
MrChrisse <chrilei2000@gmail.com>
2021-06-13 11:50:49 +0200

    finished chapter30

--- 29_Classes_OOP/app.js
+++ 29_Classes_OOP/app.js
@@ -15,3 +15,98 @@ function makeColor(r, g, b) {
 }
 
 const firstColor = makeColor(35, 255, 150);
+
+//Constructor with functions
+function Color(r, g, b) {
+  this.r = r;
+  this.g = g;
+  this.b = b;
+}
+
+Color.prototype.rgb = function () {
+  const { r, g, b } = this;
+  return `rgb(${r}, ${g}, ${b})`;
+};
+
+Color.prototype.rgba = function (a = 1.0) {
+  const { r, g, b } = this;
+  return `rgba(${r}, ${g}, ${b}, ${a})`;
+};
+
+//Constructor better with classes
+class Color2 {
+  constructor(r, g, b, name) {
+    this.r = r;
+    this.g = g;
+    this.b = b;
+    this.name = name;
+    this.rgbToHsl();
+  }
+  innerRGB() {
+    const { r, g, b } = this;
+    return `${r}, ${g}, ${b}`;
+  }
+  rgb() {
+    return `rgb(${this.innerRGB()})`;
+  }
+
+  hex() {
+    const { r, g, b } = this;
+    return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+  }
+
+  hsl() {
+    const { h, s, l } = this;
+    return `hsl(${h}, ${s}%, ${l}%)`;
+  }
+
+  fullSaturation() {
+    const { h, l } = this;
+    return `hsl(${h}, 100%, ${l}%)`;
+  }
+
+  opposite() {
+    const { h, s, l } = this;
+    const newHue = (h + 180) % 360;
+    return `hsl(${newHue}, ${s}%, ${l}%)`;
+  }
+
+  rgbToHsl() {
+    let { r, g, b } = this;
+    (r /= 255), (g /= 255), (b /= 255);
+    var max = Math.max(r, g, b),
+      min = Math.min(r, g, b);
+    var h,
+      s,
+      l = (max + min) / 2;
+
+    if (max == min) {
+      h = s = 0; // achromatic
+    } else {
+      var d = max - min;
+      s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+      switch (max) {
+        case r:
+          h = (g - b) / d + (g < b ? 6 : 0);
+          break;
+        case g:
+          h = (b - r) / d + 2;
+          break;
+        case b:
+          h = (r - g) / d + 4;
+          break;
+      }
+      h /= 6;
+    }
+
+    l = l * 100;
+    s = s * 100;
+
+    this.h = h;
+    this.s = s;
+    this.l = l;
+  }
+}
+
+let c1 = new Color2(134, 44, 55);
+let white = new Color2(255, 255, 255);
