b3a40c46b399ffe6e7cf9a2adfcbe4f6e978f177
MrChrisse <chrilei2000@gmail.com>
2021-07-01 21:28:20 +0200

    continued

--- 35_REST_Routes/index.js
+++ 35_REST_Routes/index.js
@@ -1,11 +1,43 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-  res.render('getpost');
+const comments = [
+  {
+    username: 'Todd',
+    comment: 'Lol',
+  },
+  {
+    username: 'Ted',
+    comment: 'Lel',
+  },
+  {
+    username: 'Tid',
+    comment: 'Lil',
+  },
+];
+
+// app.get('/', (req, res) => {
+//   res.render('getpost');
+// });
+
+app.get('/comments', (req, res) => {
+  res.render('comments/index', { comments });
+});
+
+app.get('/comments/new', (req, res) => {
+  res.render('comments/new');
+});
+
+app.post('/comments', (req, res) => {
+  const { username, comment } = req.body;
+  comments.push({ username, comment });
+  res.redirect('comments');
 });
 
 app.get('/tacos', (req, res) => {
