359f0717bbaf1de86c11bb26620752fb8f532ee7
Christian Leingang <chrilei2000@gmail.com>
2021-09-13 22:10:41 +0200

    test from new PC

--- 37_Mongoose/index.js
+++ 37_Mongoose/index.js
@@ -14,3 +14,5 @@ const Movie = mongoose.model('Movie', movieSchema);
 const amadeus = new Movie({ title: 'Amadeus', year: 1986, score: 9.2, rating: 'R' });
 
 amadeus.save().then(() => console.log('Inserted Movie'));
+
+//test
