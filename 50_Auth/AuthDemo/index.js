2bab4ad59c24172e3326815f297f0afb4ac8e8ff
Christian Leingang <chrilei2000@gmail.com>
2021-11-15 21:58:20 +0100

    finished chapter50

--- 50_Auth/AuthDemo/index.js
+++ 50_Auth/AuthDemo/index.js
@@ -1,18 +1,73 @@
 const express = require('express');
-
 const app = express();
-
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const session = require('express-session');
 const User = require('./models/user');
+
+mongoose
+  .connect('mongodb://localhost:27017/authdemo', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('MONGO CONNECTION OPEN!!!');
+  })
+  .catch((err) => {
+    console.log('OH NO MONGO CONNECTION ERROR!!!!');
+    console.log(err);
+  });
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(session({ secret: 'secret' }));
+
+const requireLogin = (req, res, next) => {
+  if (!req.session.user_id) {
+    return res.redirect('/login');
+  }
+  next();
+};
+
+app.get('/', (req, res) => {
+  res.send('Home');
+});
 
 app.get('/register', (req, res) => {
   res.render('register');
 });
 
-app.get('/secret', (req, res) => {
-  res.send('Cant see me');
+app.post('/register', async (req, res) => {
+  const { password, username } = req.body;
+  const hash = await bcrypt.hash(password, 12);
+  const user = new User({ username, password });
+  await user.save();
+  req.session.user_id = user._id;
+  res.redirect('/');
+});
+
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+app.post('/login', async (req, res) => {
+  const { password, username } = req.body;
+  const foundUser = await User.findAndValidate(username, password);
+  if (foundUser) {
+    req.session.user_id = foundUser._id;
+    res.redirect('/secret');
+  } else {
+    res.redirect('/login');
+  }
+});
+
+app.post('/logout', (req, res) => {
+  //req.session.user_id = null;
+  req.session.destroy();
+  res.redirect('/login');
+});
+
+app.get('/secret', requireLogin, (req, res) => {
+  res.render('secret');
 });
 
 app.listen(3000, () => {
